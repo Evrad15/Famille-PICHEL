d3a5d23f3e731bf312b25346c1cc476684e5b4db
MOPI EVRAD <mopisonkouat00@gmail.com>
2026-03-22 21:36:35 +0100

    vite update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' // Si tu es en v4
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-
 })
